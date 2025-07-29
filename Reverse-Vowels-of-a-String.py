5c152f875f1c6b31e0700b9772477f5b8513e518
Shashank Amireddy <132253252+shashank-amireddy@users.noreply.github.com>
2025-07-30 01:21:19 +0530

    [345] [Time Beats: 32.06%] [Memory Beats: 27.79%] - LeetPush

--- Reverse-Vowels-of-a-String.py
+++ Reverse-Vowels-of-a-String.py
@@ -1,19 +1,15 @@
 class Solution:
-    def reverseVowels(self, s: str) -> str:
-        vowels = ['a', 'e', 'i', 'o', 'u']
-        s_list = list(s)  
-        vow_indices = []
-        
-        for i, char in enumerate(s_list):
-            if char.lower() in vowels:
-                vow_indices.append(i)
-        
-        i, j = 0, len(vow_indices) - 1
-        while i < j:
-            s_list[vow_indices[i]], s_list[vow_indices[j]] = s_list[vow_indices[j]], s_list[vow_indices[i]]
-            i += 1
-            j -= 1
-        
-        return "".join(s_list)
+    def reverseVowels(self, l: str) -> str:
+        v = ['a','e','i','o','u']
+        vow = []
+        s=list(l)
+        for i in range(len(s)):
+            if (s[i].lower() in v):
+                vow.append(s[i])
+                s[i]=None
 
+        for i in range(len(s)):
+            if s[i] == None:
+                s[i] = vow.pop()
+        return "".join(s)
         
